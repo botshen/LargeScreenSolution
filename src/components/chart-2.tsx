03352667e6e996541c22865f6e4f7bcb67999b0f
shenxin <363088847@qq.com>
2021-07-25 11:33:04 +0800

    制作 footer

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useRef} from 'react';
 import * as echarts from 'echarts';
+import {px} from '../shared/px';
+import {baseEchartOptions} from '../shared/base-echart-options';
 import {createEchartsOptions} from '../shared/create-echarts-options';
 
 export const Chart2 = () => {
@@ -28,12 +30,34 @@ export const Chart2 = () => {
                 {
                     name: '2011年',
                     type: 'bar',
-                    data: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                    data: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    itemStyle: {
+                        normal: {
+                            color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                                offset: 0,
+                                color: '#2034f9'
+                            }, {
+                                offset: 1,
+                                color: '#04a1ff'
+                            }]),
+                        }
+                    }
                 },
                 {
                     name: '2012年',
                     type: 'bar',
-                    data: [2, 3, 4, 5, 6, 7, 8, 9, 10]
+                    data: [2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    itemStyle: {
+                        normal: {
+                            color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                                offset: 0,
+                                color: '#b92ae8'
+                            }, {
+                                offset: 1,
+                                color: '#6773e7'
+                            }]),
+                        }
+                    }
                 }
             ]
         }));
@@ -42,11 +66,10 @@ export const Chart2 = () => {
     return (
         <div className="bordered 破获排名">
             <h2>案件破获排名</h2>
-            <div ref={divRef} className="chart">
-                <div className="legend">
-                    <span className="first"/> 破案排名1
-                    <span className="second"/> 破案排名1
-                </div>
+            <div ref={divRef} className="chart"/>
+            <div className="legend">
+                <span className="first"/> 破案排名1
+                <span className="second"/> 破案排名2
             </div>
         </div>
     );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,9 +4,8 @@ import headerBg from '../images/header.png';
 import {Chart1} from '../components/chart-1';
 import {Chart2} from '../components/chart-2';
 
-
 export const Home = () => {
-
+    const year = new Date().getFullYear();
     return (
         <div className="home">
             <header style={{backgroundImage: `url(${headerBg})`}}/>
@@ -15,11 +14,15 @@ export const Home = () => {
                     <Chart1/>
                     <Chart2/>
                 </section>
-                <section className="bordered section2"></section>
+                <section className="bordered section2">
+                </section>
                 <section className="bordered section3"></section>
                 <section className="bordered section4"></section>
                 <section className="bordered section5"></section>
             </main>
+            <footer>
+                &copy; 饥人谷 2020-{year}
+            </footer>
         </div>
     );
 };
